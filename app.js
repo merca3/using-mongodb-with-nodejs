c9b0d8943e494135367b3615758d6bc7e4c39e1f
Katrina Merca <78380665+merca3@users.noreply.github.com>
2025-03-21 16:09:45 +0200

    Add circulation repo with loadData, get, filter, limit and assert

--- app.js
+++ app.js
@@ -1,15 +1,41 @@
 const MongoClient = require("mongodb").MongoClient;
+const assert = require("assert");
+
+const circulationRepo = require("./repos/circulationRepo");
+const data = require("./circulation.json");
 
 const url = "mongodb://localhost:27017";
 const dbName = "circulation";
 
 async function main() {
   const client = new MongoClient(url);
-  await client.connect();
 
-  const admin = client.db(dbName).admin();
-  console.log(await admin.serverStatus());
-  console.log(await admin.listDatabases());
+  try {
+    await client.connect();
+
+    const results = await circulationRepo.loadData(data);
+    assert.equal(data.length, results.insertedCount);
+
+    const getData = await circulationRepo.get();
+    assert.equal(data.length, getData.length);
+
+    const filterData = await circulationRepo.get({
+      Newspaper: getData[4].Newspaper,
+    });
+    assert.deepEqual(filterData[0], getData[4]);
+
+    const limitData = await circulationRepo.get({}, 3);
+    assert.equal(limitData.length, 3);
+  } catch (error) {
+    console.log(error);
+  } finally {
+    const admin = client.db(dbName).admin();
+
+    await client.db(dbName).dropDatabase();
+    console.log(await admin.listDatabases());
+
+    client.close();
+  }
 }
 
 main();
---
+++ repos/circulationRepo.js
@@ -0,0 +1,48 @@
+const { MongoClient } = require("mongodb");
+
+function circulationRepo() {
+  const url = "mongodb://localhost:27017";
+  const dbName = "circulation";
+
+  function get(query, limit) {
+    return new Promise(async (resolve, reject) => {
+      const client = new MongoClient(url);
+
+      try {
+        await client.connect();
+        const db = client.db(dbName);
+
+        let items = db.collection("newspapers").find(query);
+        if (limit > 0) {
+          items = items.limit(limit);
+        }
+        resolve(await items.toArray());
+        client.close();
+      } catch (error) {
+        reject(error);
+      }
+    });
+  }
+
+  function loadData(data) {
+    return new Promise(async (resolve, reject) => {
+      const client = new MongoClient(url);
+
+      try {
+        await client.connect();
+        const db = client.db(dbName);
+
+        results = await db.collection("newspapers").insertMany(data);
+
+        resolve(results);
+        client.close();
+      } catch (error) {
+        reject(error);
+      }
+    });
+  }
+
+  return { loadData, get };
+}
+
+module.exports = circulationRepo();
